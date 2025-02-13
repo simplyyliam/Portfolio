6fd0b87ae677f84d1ec4786ca2f43c3dac7bc78f
simplyyliam <simplyliam.dev@gmail.com>
2025-02-13 17:15:43 +0200

    Added Portfolio to vercel

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,13 +3,18 @@ import { FeaturedNav } from "../components/Projects/FeaturedNav";
 
 
 export function Projects () {
+    
+    const onProject = () => {
+
+    }
+    
   return (
     <div className="flex w-screen h-screen items-center justify-center relative">
       <section className="md:w-[46em] md:h-[30em] sm:w-[41em] sm:h-[24em] w-[21em] h-[14em] gap-20 overflow-hidden hover:scale-110 transition-transform ease-linear bg-stone-200 rounded-3xl">
         
-        <FeaturedCards/>
+        <FeaturedCards imageSRC="" to=''/>
       </section>
-        <FeaturedNav/>
+        <FeaturedNav onProjects={onProject}/>
     </div>
   );
 }
