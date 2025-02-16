0f696de40ece82395b74b06808329eeb8a63491c
simplyyliam <simplyliam.dev@gmail.com>
2025-02-16 12:57:08 +0200

    fixing mobile responsive design

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -11,20 +11,26 @@ export function Landing_Hero () {
         const mm = gsap.matchMedia()
     
         mm.add("(max-width: 600px)", () => {
-            gsap.to(HeroRef.current, {height: 700, width: 400, delay: 0.3})
+            gsap.to(HeroRef.current, {height: 700, width: 400, delay: 0.3, borderRadius: 20})
             gsap.to(HeaderRef.current, {
                 scale: 1.5, // Scale the text
-                text: "Scaling and Typing!", // Change the text content
+                duration: 2,
+                ease: "elastic.out(1, 0.75)", // Elastic effect for scaling
+            })
+        })
+        mm.add("(max-width: 375px)", () => {
+            gsap.to(HeroRef.current, {height: 600, width: 300, delay: 0.3, borderRadius: 20})
+            gsap.to(HeaderRef.current, {
+                scale: 1.5, // Scale the text
                 duration: 2,
                 ease: "elastic.out(1, 0.75)", // Elastic effect for scaling
             })
         })
 
-        mm.add('(min-width: 769px)', () => {
-            gsap.to(HeroRef.current, {height: 700, width: 1300, delay: 0.3})
+        mm.add('(min-width: 992px)', () => {
+            gsap.to(HeroRef.current, {height: 700, width: 1300, delay: 0.3, borderRadius: 20})
             gsap.to(HeaderRef.current, {
                 scale: 1.5, // Scale the text
-                text: "Scaling and Typing!", // Change the text content
                 duration: 2,
                 ease: "elastic.out(1, 0.75)", // Elastic effect for scaling
             }) 
@@ -38,7 +44,7 @@ export function Landing_Hero () {
         <div ref={HeroRef} className="Hero flex w-full h-full bg-stone-300 p-5 absolute">
             <div className="flex items-center justify-center w-full h-full  bg-[#ffffff1] backdrop-blur-[5px]">
                 <div className="flex flex-col gap-6">
-                    <h1 ref={HeaderRef} className="text-white md:text-3xl  md:w-fit text-[0.65em] text-center  font-medium scale-0">Frontend Developer Building, One Pixel at a Time</h1>
+                    <h1 ref={HeaderRef} className="text-white md:text-3xl  md:w-fit text-[0.5em] text-center  font-medium scale-0">Frontend Developer Building, One Pixel at a Time</h1>
                 </div>
             </div>
         </div>
